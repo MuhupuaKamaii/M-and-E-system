3662a27a9831657b0e1aa71e464b2c703b23cfea
gabadhiya <mattindeshitila15@gmail.com>
2025-11-17 09:09:23 +0200

    Frontend Login and Home Page Ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./Test/pages/HomePage";
 
 
 import Test_Login from "./Test/login/Login"
@@ -11,12 +12,15 @@ import AddUser from "./Test/pages/AddUser";
 import RoleRoute from "./Test/components/RoleRoute";
 
 
+
+
 function App() {
 
   return (
   <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Test_Login />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Login" element={<Test_Login />} />
         <Route path="/oma-dashboard" element={<OMA_Dashboard />} />
         <Route path="/npc-dashboard" element={<NPC_Dashboard />} />
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
